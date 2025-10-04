546d1235fcc0e748df505914941f8fbd3a3f2387
Paulo Duarte <phduarte87@outlook.com>
2025-10-04 10:48:39 -0300

    upgrade dotnet

--- Inverse.Plugin.DatabaseGenerator.MsSqlServer/SqlServerDatabaseGeneratorStrategy.cs
+++ Inverse.Plugin.DatabaseGenerator.MsSqlServer/SqlServerDatabaseGeneratorStrategy.cs
@@ -1,8 +1,8 @@
 ﻿using Inverse.Domain;
+using Microsoft.Data.SqlClient;
 using System;
 using System.Collections.Generic;
 using System.Data;
-using System.Data.SqlClient;
 using System.Linq;
 
 namespace Inverse.Plugin.DatabaseGenerator.MsSqlServer
